093e113f00e6364dc1e7f6ef795295d6c1b3b7e3
Takuya Ito <ito0804takuya@gmail.com>
2024-12-22 21:12:24 +0900

    go tips

--- main.go
+++ main.go
@@ -1,28 +1,38 @@
 package main
 
 import (
-	"net/http"
-	"net/http/httptest"
-	"testing"
-
-	"github.com/labstack/echo/v4"
+	"fmt"
 )
 
-func initServer(e *echo.Echo) http.Handler {
-	e.GET("/fortune", func(c echo.Context) error {
-		c.Response().Header().Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-		return c.JSON(http.StatusOK, `{"fortune": "大吉}`)
-	})
-	return e
+type Customer struct {
+	ID      string
+	Balance float64
 }
 
 func main() {
-	e := echo.New()
-	e = initServer(e)
-	e.Logger.Fatal(e.Start(":1323"))
-}
+	s1 := []int{1, 2, 3}
+	// バグる
+	// s2 := s1[:2]
+	// 完全スライス式で容量も指定することで、s2を変更したときの影響範囲を先頭２つの要素のみに絞ることができる
+	s2 := s1[:2:2]
+	s3 := append(s2, 10)
+	fmt.Println(s1, len(s1), cap(s1))
+	fmt.Println(s2, len(s2), cap(s2))
+	fmt.Println(s3, len(s3), cap(s3))
 
-func test(t *testing.T) {
-	e := echo.New()
-	httptest.NewServer(e)
+	ints := []int{1, 2, 3}
+	for i, v := range ints {
+		fmt.Println(i, &v, &ints[i])
+	}
+	fmt.Println(ints)
+
+	bytes := []byte{'a', 'b'}
+	strings := []string{"A", "B"}
+	fmt.Println(bytes, string(bytes))
+	fmt.Println(strings)
+
+	hello := "Hello, 田中, World"
+	fmt.Println(hello[:5])
+	fmt.Println(hello[:9])
+
 }
